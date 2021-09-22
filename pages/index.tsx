1de75f1e866fea75daf4bc1fd52d2616ba01385f
samumirandam <samuelchampi@gmail.com>
2021-09-21 19:47:45 -0500

    se actualizan los fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
+
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
@@ -7,8 +9,7 @@ const HomePage = () => {
   const [productList, setProductList] = useState<TProduct[]>([])
 
   useEffect(() => {
-    window
-      .fetch('/api/avo')
+    fetch('/api/avo')
       .then((response) => response.json())
       .then(({ data }: TAPIAvoResponse) => {
         setProductList(data)
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,35 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import fetch from 'isomorphic-unfetch'
 
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
 
 const ProductPage = () => {
   const { query } = useRouter()
-<<<<<<< HEAD
-  const [product, setProduct] = useState<TProduct>()
-
-  useEffect(() => {
-    window
-      .fetch(`/api/avo/${query.id}`)
-      .then((response) => response.json())
-      .then((entry) => {
-        setProduct(entry)
-      })
-  }, [])
-
-  return (
-    <section>
-      <h1>Página producto: {query.id}</h1>
-      <p>{product?.name}</p>
-    </section>
-=======
   const [product, setProduct] = useState<TProduct | null>(null)
 
   useEffect(() => {
     if (query.id) {
-      window
-        .fetch(`/api/avo/${query.id}`)
+      fetch(`/api/avo/${query.id}`)
         .then((response) => response.json())
         .then((data: TProduct) => {
           setProduct(data)
@@ -41,7 +23,6 @@ const ProductPage = () => {
     <Layout>
       {product == null ? null : <ProductSummary product={product} />}
     </Layout>
->>>>>>> 4-ui
   )
 }
 
